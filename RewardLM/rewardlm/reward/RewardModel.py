3a8634509c52e2d462f9efc639e7c2fb3a00f81b
daniel <scalena99@gmail.com>
2023-05-09 16:17:46 +0200

    refactor repo, WIP

---
+++ RewardLM/rewardlm/reward/RewardModel.py
@@ -0,0 +1,38 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from tqdm import tqdm
+from src.utils import device_selector
+
+class RewardModel:
+    def __init__(self, model_id, device: str) -> None:
+
+        if device is None:
+            self.device = device_selector()
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_id)
+    
+    def get_score(self, model_loader: torch.utils.data.DataLoader):
+        result_tox = {
+            'prompt_score': [],
+            'response_score': [],
+        }
+
+        for _, prompt, response in tqdm(model_loader):
+            for ele1, ele2 in zip(prompt, response):
+                prompt[ele1] = prompt[ele1].to(self.device)
+                response[ele2] = response[ele2].to(self.device)
+            
+            self.model.to(self.device)
+            with torch.no_grad():
+                output_prompt = self.model(**prompt)
+                output_response = self.model(**response)
+
+            # apply softmax and selecting only scores [1]
+            out_soft_prompt = torch.nn.functional.softmax(output_prompt[0].detach(), dim = 1).cpu().numpy()[:, 1]
+            out_soft_response = torch.nn.functional.softmax(output_response[0].detach(), dim = 1).cpu().numpy()[:, 1]
+
+            result_tox['prompt_score'].extend(out_soft_prompt.tolist())
+            result_tox['response_score'].extend(out_soft_response.tolist())
+
+        return result_tox
