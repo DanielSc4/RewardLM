2d3e53d80df80192e8b87c76bdaa5a9539179173
daniel <scalena99@gmail.com>
2023-07-13 11:16:40 +0200

    Update HPC job

--- scripts_hpc/tox_meter_red.sh
+++ scripts_hpc/tox_meter_red.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=tox_meter_red
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --mem=30GB
 #SBATCH --gpus-per-node=a100.20gb:1
 #SBATCH --output=/home1/p313544/slurm_logs/%x.%j.out
