14ff01778d1178713cedfc53b721f9bd0eb2aa2e
daniel <scalena99@gmail.com>
2023-07-15 00:02:29 +0200

    new results w/ `perspective API`

--- rewardlm/data/prospectiveAPI_script.py
+++ rewardlm/data/prospectiveAPI_script.py
@@ -6,11 +6,10 @@ import time
 from googleapiclient import discovery
 from argparse import ArgumentParser
 
-
 ATTRIBUTE = 'TOXICITY'
 NAME_NEW_COL = 'pro_API_response_score'
-TIME_INTERVAL = .95      # prospective API limit the QPS (query per second) to 1
-# https://console.cloud.google.com/iam-admin/quotas to check quota alerts
+TIME_INTERVAL = 0.7      # prospective API limit the QPS (query per second) to 1 # https://console.cloud.google.com/iam-admin/quotas to check quota alerts
+TRIALS = 2      # number of times to iterate on the entire dataset
 
 
 def read_credential():
@@ -19,7 +18,8 @@ def read_credential():
     
     return cfg['google_cloud']
 
-
+def get_status(df):
+    return f"{sum(df[NAME_NEW_COL] != -1)}/{len(df)}"
 
 def main(path_to_df):
     API_KEY = read_credential()
@@ -35,64 +35,61 @@ def main(path_to_df):
     
     df = pd.read_csv(path_to_df, index_col=0)
     # create column w/ -1 scores as default
-    df[NAME_NEW_COL] = -1
+    if NAME_NEW_COL not in df.columns:
+        df[NAME_NEW_COL] = -1
 
     consecutive_err = 0
     count = 0       # total values obtained during execution
 
-    pbar = pbar = tqdm(enumerate(df.iterrows()), desc='status')
-    for it, (idx, row) in pbar:
-        n_response = 0          # total values obtained during this cycle
-        # if score is not assigned:
-        if row[NAME_NEW_COL] < 0:
-            time.sleep(TIME_INTERVAL)
-            analyze_request = {
-                'comment': { 'text': row['responses']},
-                'requestedAttributes': {ATTRIBUTE: {},}
-            }
-            # prevent HTTP errors
-            try:
-                response = client.comments().analyze(body=analyze_request).execute()
-                # assign score
-                df.at[idx, NAME_NEW_COL] = response['attributeScores'][ATTRIBUTE]['summaryScore']['value']
-                n_response += 1
-                count += 1
-                consecutive_err = 0     # reset the counter for no response in a row
-            except:
-                # if HTTP error, pass and try next time
-                pass
+
+    print(f"Restored from {get_status(df)}")
+    n_response = 0
+
+    for _ in range(TRIALS):
+        pbar = pbar = tqdm(enumerate(df.iterrows()), desc=f"status: {get_status(df)} | from last backup: {n_response} | last_backup: NaN")
         
-        # exit loop 
-        if sum(df[NAME_NEW_COL] == -1) == 0:
-            # no more scores to get
-            break
+        for it, (idx, row) in pbar:
+            # if score is not assigned:
+            if row[NAME_NEW_COL] < 0:
+                time.sleep(TIME_INTERVAL)
+                analyze_request = {
+                    'comment': { 'text': row['responses']},
+                    'requestedAttributes': {ATTRIBUTE: {},}
+                }
 
-        # if, for more than 5 times in a row, the server does not provide any response, pause for 5 seconds
-        # if, for more than 20 times in a row, the server does not provide any response, quit
-        if n_response == 0:     # no responses obtained during this cycle
-            consecutive_err += 1
-
-            if consecutive_err > 5:
-                time.sleep(4)
-
-                # backup
-                df.to_csv(
-                    '{original_path_and_name}_w_pAPI.csv'.format(original_path_and_name = path_to_df.split('.csv')[0])
-                )
-                pbar.set_description(f"{sum(df[NAME_NEW_COL] != -1)}/{len(df)}, last_backup: {it}")
-
-            if consecutive_err > 20:
-                print('\nServer is not providing any response, 5 time in a row. Bye!')
-                print(f'Num of missing values: {sum(df[NAME_NEW_COL] == -1)}/{len(df)}')
+                # catch HTTP errors
+                try:
+                    response = client.comments().analyze(body=analyze_request).execute()
+                    # assign score
+                    df.at[idx, NAME_NEW_COL] = response['attributeScores'][ATTRIBUTE]['summaryScore']['value']
+                    n_response += 1
+                    count += 1
+                    consecutive_err = 0     # reset the counter for no response in a row
+                except Exception as e:
+                    if 'LANGUAGE_NOT_SUPPORTED_BY_ATTRIBUTE' in str(e):
+                        df.at[idx, NAME_NEW_COL] = pd.NA
+                    # if HTTP error, pass and try next time
+                    pass
+            
+            # exit loop 
+            if sum(df[NAME_NEW_COL] == -1) == 0:
+                print('\n[x] No more scores to get')
                 break
+            
+            # backup, overwrite
+            if it % 100 == 0:
+                df.to_csv(path_to_df)
+                pbar.set_description(f"status: {get_status(df)} | from last backup: {n_response} | last_backup: {it}it")
+                n_response = 0
 
-    print(f'total scores obtained this time: {count}')
-    print(f'total valid scores in df: {sum(df[NAME_NEW_COL] != -1)}/{len(df)}')
+
+        print(f'total scores obtained this loop: {count}')
+        print()
+    
+    print(f'total scores in df: {get_status(df)}')
+    print(f'total valid scores in df: {sum(df[NAME_NEW_COL] > 0)}/{len(df)}')
     # save csv
-    df.to_csv(
-        '{original_path_and_name}_w_pAPI.csv'.format(original_path_and_name = path_to_df.split('.csv')[0])
-    )
-
+    df.to_csv(path_to_df)
 
 
 
@@ -104,5 +101,6 @@ if __name__ == "__main__":
                         help='Path to the CSV file')
     args = parser.parse_args()
     path_to_df = args.path
+
 
     main(path_to_df)
