a2d1513baf2b40d614cd0b719ac6c54036e64ef0
daniel <scalena99@gmail.com>
2023-07-16 15:56:21 +0200

    Update on `config` for `falcon7B-[FT, RL]`

--- scripts_hpc/tox_meter_falcon.sh
+++ scripts_hpc/tox_meter_falcon.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=tox_meter_falcon
 #SBATCH --time=06:00:00
 #SBATCH --mem=30GB
-#SBATCH --gpus-per-node=a100:1
+#SBATCH --gpus-per-node=a100.20gb:1
 #SBATCH --output=/home1/p313544/slurm_logs/%x.%j.out
 
 
