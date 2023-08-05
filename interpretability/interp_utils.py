58888ccc85c979418d209e87e96443b3ba1a2d6c
daniel <scalena99@gmail.com>
2023-08-05 19:56:24 +0200

    calculating prompt_dependancy

--- interpretability/interp_utils.py
+++ interpretability/interp_utils.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+
+from inseq import FeatureAttributionOutput
+
 
 
 
@@ -22,3 +26,34 @@ def stratify_df(df: pd.DataFrame, subset_size):
             ) for lbl, group in df.groupby('label')
     ])
     return stratified_df
+
+
+def get_prompt_dependancy(attributions: FeatureAttributionOutput, max_n_tok: int = 50):
+    """Compute prompt token level dependancy for each generation in attributions
+
+    Args:
+        attributions (FeatureAttributionOutput): attributions used to compute the prompt token level dependancy
+        max_n_tok (int, optional): number of maximum generated tokens where dependancy is computed. Defaults to 50.
+
+    Returns:
+        np.array: numpy array [len(attributions), max_n_tok]
+    """
+
+    final = []
+    for attr in attributions:
+        if not hasattr(attr, '_aggregator'):
+            # attribution has not been previously aggregated
+            attr = attr.aggregate()
+        
+        attr_matrix = np.nan_to_num(attr.target_attributions)
+        final.append((
+                np.array([
+                    attr_matrix[:len(attr.source), n_tok].sum().item()      # sum the column for each generated token (entrire column (prompt + gen) sums to one)
+                    if n_tok < attr_matrix.shape[1] else np.nan             # nan is left where generations length is less than max_n_tok
+                    for n_tok in range(max_n_tok)       # each generated token
+                ])
+            ))
+    
+    # final = np.nan_to_num(np.vstack(final))
+
+    return final
