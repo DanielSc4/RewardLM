15fc6a20e466a20cd28fcf533baea3301e69d062
daniel <scalena99@gmail.com>
2023-08-07 16:28:42 +0200

    Improved plots

--- interpretability/interp_utils.py
+++ interpretability/interp_utils.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 
 from inseq import FeatureAttributionOutput
 
+# plot color palette
+palette = ['#2D3047', '#576490', '#7796CB', '#A3BCF9', '#D1D2F9', '#C9CAD9', '#B7B8C5', '#FCB97D']
+diversity_palette = ['#2D3047', '#7796CB', '#FCB97D', '#E54F6D', '#79C99E', '#A9F0D1',]
+diversity_palette_ord = ['#7796CB', '#79C99E', '#FCB97D', '#E54F6D', '#2D3047', '#A9F0D1',]
+
 
 def _assign_label(value):
     if value < 0.333:
@@ -59,18 +64,21 @@ def get_prompt_dependancy(attributions: FeatureAttributionOutput, max_n_tok: int
     return np.array(final)
 
 
-def get_plot_prompt_dep_toxicity(dependancies: np.array, attr_labels: np.array, fig_kwargs: dict):
+def get_plot_prompt_dep_toxicity(dependancies: np.array, attr_labels: np.array, model_name: str, fig_kwargs: dict = None):
     r"""Plot average prompt dependancy, distinguishing between the bucket in `attr_labels`.
 
     Args:
         dependancies (`np.array`): prompt dependancies obtained from `get_prompt_dependancy` fun.
         attr_labels (`np.array`): array with shape `(n, 1)` where `n` is the number of attributions.
+        model_name (`str`): name of the model (title).
         fig_kwargs (`dict`): figure kwargs.
 
     Returns:
         `matplotlib.pyplot`: same as description.
-    """
+    """ 
 
+    local_palette = diversity_palette[3], diversity_palette[4], diversity_palette[1]
+    
     if attr_labels[0]:
         assert dependancies.shape[0] == len(attr_labels), f"Number of dependancies (0 dim of dependancies: {dependancies.shape[0]}) must be equal to the number of given labels ({len(attr_labels)})"
 
@@ -78,34 +86,85 @@ def get_plot_prompt_dep_toxicity(dependancies: np.array, attr_labels: np.array, 
 
     if not fig_kwargs:
         fig_kwargs = {
-            'figsize': (10, 5),
-            'dpi': 300,
+            'figsize': (9, 6),
+            'dpi': 150,
         }
 
-    plt.figure(**fig_kwargs)
-    plt.title('Generation prompt dependancy, avg per toxicity level')
-    for i, lbl in enumerate(np.unique(attr_labels)):
-        plt.plot(
-            np.nanmean(dependancies[(attr_labels == np.unique(attr_labels))[:, i]], axis = 0),
+    fig, ax = plt.subplots(**fig_kwargs)
+    ax.set_title(f'[{model_name}], Generation prompt dependancy, avg per toxicity level')
+
+    for i, (lbl, color) in enumerate(zip(np.unique(attr_labels), local_palette)):
+        group = dependancies[(attr_labels == np.unique(attr_labels))[:, i]]
+
+        avgs = np.nanmean(group, axis = 0)
+        # ci = (1.96 for 95%, 2.58 for 99%) * z * (std / sqrt(len(data)))  # counting how many not na values there are
+        z = 1.96
+        offsets = z * np.nanstd(group, axis = 0) / np.sqrt(np.invert(np.isnan(group)).sum(axis = 0))
+        
+        ax.plot(
+            np.arange(0, len(avgs)),
+            avgs,
             label = lbl,
+            color = color,
         )
-    plt.legend()
-    plt.xlabel(r'$n$ generated tokens')
-    plt.ylabel('prompt dependancy (sum)')
-    plt.grid(alpha = .3)
+        ax.fill_between(
+            np.arange(0, len(avgs)),
+            (avgs - offsets),
+            (avgs + offsets),
+            color = color, alpha = .08,
+            # label = f'{.95 if z == 1.96 else "z = " + z } confidence interval',
+        )
+
+    ax.set_ylim(0, 1.1)
+    ax.legend()
+    ax.set_xlabel(r'$n$ generated tokens')
+    ax.set_ylabel('prompt dependancy (sum)')
+    ax.grid(alpha = .3, linestyle = ':')
     return plt
 
 
 
 
-def get_plot_compare_models(dependencies: dict, fig_kwargs: dict):
+def get_plot_training_compare(dependencies: dict, model_name:str, fig_kwargs: dict = None):
+    
+    # check
+    exp_keys = ['PT', 'FT', 'RL']
+    # assert set(exp_keys) == set(attribution.keys()), f'dependencies must be a dict with {exp_keys} keys. Got {list(attribution.keys())}'
 
     if not fig_kwargs:
         fig_kwargs = {
-            'figsize': (10, 5),
-            'dpi': 300,
+            'figsize': (9, 6),
+            'dpi': 150,
         }
+    
+    fig, ax = plt.subplots(**fig_kwargs)
+    ax.set_title(f'[{model_name}], Prompt dependancy, PT vs FT vs RL')
 
+
+    for k, color in zip(dependencies, diversity_palette_ord):
+        avgs = np.nanmean(dependencies[k], axis = 0)
+        # ci = (1.96 for 95%, 2.58 for 99%) * z * (std / sqrt(len(data)))  # counting how many not na values there are
+        z = 1.96
+        offsets = z * np.nanstd(dependencies[k], axis = 0) / np.sqrt(np.invert(np.isnan(dependencies[k])).sum(axis = 0))
+
+        ax.plot(
+            np.arange(0, len(avgs)),
+            avgs,
+            label = k,
+            color = color
+        )
+        ax.fill_between(
+            np.arange(0, len(avgs)),
+            (avgs - offsets),
+            (avgs + offsets),
+            color = color, alpha = .15,
+            # label = f'{.95 if z == 1.96 else "z = " + z } confidence interval',
+        )
     
+    ax.set_ylim(0, 1.1)
+    ax.set_xlabel(r'$n$ generated tokens')
+    ax.set_ylabel('prompt dependancy (sum)')
+    ax.legend()
+    ax.grid(alpha = .3, linestyle = ':')
     
-    return
+    return plt
