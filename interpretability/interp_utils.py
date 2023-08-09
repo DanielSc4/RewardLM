10199c1bb622b0c4790009c1a7e12af4186154f4
daniel <scalena99@gmail.com>
2023-08-09 12:02:34 +0200

    Doc and `get_plot_toxlev2toxlev` improvements

--- interpretability/interp_utils.py
+++ interpretability/interp_utils.py
@@ -36,8 +36,8 @@ def _get_offsets_ci(dependencies: np.array, z = 1.96):
     r"""Return confidence interval offsets for dependencies
 
     Args:
-        dependencies (`np.array`): dependencies of shape `(n, n_tokens)` where `n` is the number of records.
-        z (`float`, optional): Z parameter, (`1.96` for 95%, `2.58` for 99%). Defaults to `1.96`.
+        - `dependencies` (`np.array`): dependencies of shape `(n, n_tokens)` where `n` is the number of records.
+        - `z` (`float`, optional): Z parameter, (`1.96` for 95%, `2.58` for 99%). Defaults to `1.96`.
 
     Returns:
         `np.array`: array of offsets of shape `(1, n_tokens)`
@@ -52,11 +52,11 @@ def get_prompt_dependancy(attributions: FeatureAttributionOutput, max_n_tok: int
     Notice: generation dependancy can be calculated as `1 - prompt_dependancy`.
 
     Args:
-        `attributions` (FeatureAttributionOutput): attributions used to compute the prompt token level dependancy
-        `max_n_tok` (int, optional): number of maximum generated tokens where dependancy is computed. Defaults to 50.
+        - `attributions` (`FeatureAttributionOutput`): attributions used to compute the prompt token level dependancy
+        - `max_n_tok` (`int`, optional): number of maximum generated tokens where dependancy is computed. Defaults to 50.
         
     Returns:
-        `np.array`: numpy array with `[len(attributions), max_n_tok]` shape.
+        - `np.array`: numpy array with `[len(attributions), max_n_tok]` shape.
     """
 
     final = []
@@ -83,13 +83,13 @@ def get_plot_prompt_dep_toxicity(dependancies: np.array, attr_labels: np.array, 
     r"""Plot average prompt dependancy, distinguishing between the bucket in `attr_labels`.
 
     Args:
-        dependancies (`np.array`): prompt dependancies obtained from `get_prompt_dependancy` fun.
-        attr_labels (`np.array`): array with shape `(n, 1)` where `n` is the number of attributions.
-        model_name (`str`): name of the model (title).
-        fig_kwargs (`dict`): figure kwargs. Defaults to None.
+        - `dependancies` (`np.array`): prompt dependancies obtained from `get_prompt_dependancy` fun.
+        - `attr_labels` (`np.array`): array with shape `(n, 1)` where `n` is the number of attributions.
+        - `model_name` (`str`): name of the model (title).
+        - `fig_kwargs` (`dict`): figure kwargs. Defaults to None.
 
     Returns:
-        `matplotlib.pyplot`: same as description.
+        - `matplotlib.pyplot`: same as description.
     """ 
 
     local_palette = diversity_palette[3], diversity_palette[4], diversity_palette[1]
@@ -139,12 +139,12 @@ def get_plot_training_compare(dependencies: dict, model_name:str, fig_kwargs: di
     """Plot prompt dependancy comparing different type of training, Pre-Trained, Fine-Tuned and Reinforcement Learning.
 
     Args:
-        dependencies (`np.array`): prompt dependancies obtained from `get_prompt_dependancy` fun.
-        model_name (`str`): name of the model (title).
-        fig_kwargs (`dict`): figure kwargs. Defaults to None.
+        - `dependencies` (`np.array`): prompt dependancies obtained from `get_prompt_dependancy` fun.
+        - `model_name` (`str`): name of the model (title).
+        - `fig_kwargs` (`dict`): figure kwargs. Defaults to None.
 
     Returns:
-        `matplotlib.pyplot`: same as description.
+        - `matplotlib.pyplot`: same as description.
     """
     local_palette = diversity_palette[1], diversity_palette[4], diversity_palette[2], diversity_palette[3]
     
@@ -187,14 +187,40 @@ def get_plot_training_compare(dependencies: dict, model_name:str, fig_kwargs: di
     return plt
 
 
-def get_plot_toxlev2toxlev(deps, lbls, start: str, end: str, model_name: str, fig_kwargs: dict = None):
-    # assert start in lbls_1, f'start ({start}) must be in lbls_1'
-    # assert end in lbls_2, f'end ({end}) must be in lbls_2'
+def get_plot_toxlev2toxlev(deps: dict, lbls: dict, from_to: list[tuple[str]], model_name: str, fig_kwargs: dict = None):
+    r"""Plot the prompt dependancy level comparing two different training methods. 
+    The function selects all generation classified as `from_to[_][0]` by the first model
+    and generations classified as `from_to[_][1]` by the second model.
+
+    Args:
+        - `deps` (`dict`): Dict with 2 keys naming two different models and their prompt dependancy.
+        - `lbls` (`dict`): Dict with 2 keys naming two different models and their nominal labels
+        - `from_to` (`list[tuple[str]]`): list of tuples. Each tuple contains two string labels.
+        - `model_name` (`str`): Name of the model for title.
+        - `fig_kwargs` (`dict`, optional): figure kwargs. Defaults to None.
+
+    Returns:
+        - `matplotlib.pyplot`: same as description.
+    """
+    
     
     assert deps.keys() == lbls.keys(), f'deps ({deps.keys()}) and lbls ({lbls.keys()}) not having the same keys.'
-    assert len(deps.keys()) == 2, f'deps and lbls must have {2} keys ({len(deps.keys())} were given).'
+    assert len(deps.keys()) == 2, f'deps must have {2} keys ({len(deps.keys())} were given).'
+    assert len(lbls.keys()) == 2, f'lbls must have {2} keys ({len(lbls.keys())} were given).'
+    
+    assert hasattr(from_to, '__iter__'), f'from_to parameter must be a list or iterable of tuples, {type(from_to)} were given.'
+    assert all([isinstance(x, tuple) for x in from_to]), f'all of the object in from_to must be tuple, {type(from_to)[0]} were given.'
+    assert all([len(x) == 2 for x in from_to]), f'all tuples in from_to must be of len {2}, {len(from_to[0])} were given.'
 
-    local_palette = diversity_palette[1], diversity_palette[4], diversity_palette[2], diversity_palette[3]
+    first_key, second_key = lbls.keys()
+    assert [item in lbls[first_key] for item in np.unique([ele[0] for ele in from_to])], f'found key in from_to not in lbls [0 dim].'
+    assert [item in lbls[second_key] for item in np.unique([ele[1] for ele in from_to])], f'found key in from_to not in lbls [1 dim].'
+
+    local_palette = diversity_palette[1], diversity_palette[4], diversity_palette[2], diversity_palette[3], diversity_palette[0], diversity_palette[5]
+    
+    def _pairwise(iterable):
+        a = iter(iterable)
+        return zip(a, a)
 
     if not fig_kwargs:
         fig_kwargs = {
@@ -213,48 +239,54 @@ def get_plot_toxlev2toxlev(deps, lbls, start: str, end: str, model_name: str, fi
         )
         lbls[first_key], lbls[second_key] = lbls[first_key][:lowest_dim], lbls[second_key][:lowest_dim]
         deps[first_key], deps[second_key] = deps[first_key][:lowest_dim], deps[second_key][:lowest_dim]
-    indexes = ((lbls[first_key] == start) & (lbls[second_key] == end)).flatten()
     
-    if not indexes.sum() > 0:
-        print(f'No instance of {start}2{end} found.')
+    
 
-    ax.set_title(f'[{model_name}], Prompt dependancy, toxicity: {first_key}.{start} -> {second_key}.{end}')
+    ax.set_title(f'[{model_name}] {first_key} -> {second_key}, Prompt dependancy')
 
-    ## first line (start)
-    d = deps[first_key][indexes]
-    avgs = np.nanmean(d, axis = 0)
-    ax.plot(
-        np.arange(0, len(avgs)),
-        avgs,
-        label = f'{first_key} {start} toxicity',
-        color = local_palette[0],
-    )
+    
+    for (color_s, color_e), (start, end) in zip(_pairwise(local_palette), from_to):
+        indexes = ((lbls[first_key] == start) & (lbls[second_key] == end)).flatten()
+    
+        if not indexes.sum() > 0:
+            warnings.warn(f'No instance of {start}2{end} found, jumping line plot')
+        else: 
+            ## first line (start)
+            d = deps[first_key][indexes]
+            avgs = np.nanmean(d, axis = 0)
+            ax.plot(
+                np.arange(0, len(avgs)),
+                avgs,
+                label = f'{first_key} {start} -> {second_key} {end} ({first_key})',
+                color = color_s,
+                ls = '--'
+            )
 
-    offsets = _get_offsets_ci(d)
-    ax.fill_between(
-        np.arange(0, len(avgs)),
-        (avgs - offsets),
-        (avgs + offsets),
-        color = local_palette[0], alpha = .15,
-    )
+            offsets = _get_offsets_ci(d)
+            ax.fill_between(
+                np.arange(0, len(avgs)),
+                (avgs - offsets),
+                (avgs + offsets),
+                color = color_s, alpha = .12,
+            )
 
-    ## second line (end)
-    d = deps[second_key][indexes]
-    avgs = np.nanmean(d, axis = 0)
-    ax.plot(
-        np.arange(0, len(avgs)),
-        avgs,
-        label = f'{second_key} {end} toxicity',
-        color = local_palette[1],
-    )
+            ## second line (end)
+            d = deps[second_key][indexes]
+            avgs = np.nanmean(d, axis = 0)
+            ax.plot(
+                np.arange(0, len(avgs)),
+                avgs,
+                label = f'{first_key} {start} -> {second_key} {end} ({second_key})',
+                color = color_e,
+            )
 
-    offsets = _get_offsets_ci(d)
-    ax.fill_between(
-        np.arange(0, len(avgs)),
-        (avgs - offsets),
-        (avgs + offsets),
-        color = local_palette[0], alpha = .15,
-    )
+            offsets = _get_offsets_ci(d)
+            ax.fill_between(
+                np.arange(0, len(avgs)),
+                (avgs - offsets),
+                (avgs + offsets),
+                color = color_e, alpha = .12,
+            )
 
     ax.set_ylim(0.19, 1.1)
     ax.set_xlabel(r'$n$ generated tokens')
